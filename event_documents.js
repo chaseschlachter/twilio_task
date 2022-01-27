12ffcc3f6d485bed7f65bb69377e593cd6041b76
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2022-01-26 21:48:14 -0600

    Dummy data

--- event_documents.js
+++ event_documents.js
@@ -11,9 +11,12 @@
       }, 
     "event_start" : ISODate("2022-10-30T01:37:00Z"), 
     "event_end" : ISODate("2022-12-25T02:37:00Z"), 
-    "guests" : [ { 
-      "phone" : 11111, "_id" : ObjectId("61ef629981f154bead6b0de4") 
-      } ], 
+    "guests" : [ 
+      { 
+        "phone" : 11111, 
+        "_id" : ObjectId("61ef629981f154bead6b0de4") 
+       } 
+     ], 
     "created" : ISODate("2022-01-25T02:37:40.334Z"), 
     "artist" : ObjectId("610f16a6ba0d5fc2d4593093"), 
     "__v" : 1 
