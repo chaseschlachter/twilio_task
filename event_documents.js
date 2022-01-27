8f507bf1e485369c0910a92731f40487279e2a0f
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2022-01-26 21:47:11 -0600

    Database documents - dummy data

---
+++ event_documents.js
@@ -0,0 +1,20 @@
+{ 
+  "_id" : ObjectId("61ef627481f154bead6b0dd4"), 
+    "event_name" : "Upcoming Event", 
+    "venue_name" : "Little Woodrows", 
+    "address" : "100 W 5th Street Austin TX", 
+    "description" : "Future dated event at all times to test the phone number signup feature",
+    "image" : "https://test.com",
+    "geometry" : { 
+      "type" : "Point", 
+      "coordinates" : [ -97.751881, 30.26472 ] 
+      }, 
+    "event_start" : ISODate("2022-10-30T01:37:00Z"), 
+    "event_end" : ISODate("2022-12-25T02:37:00Z"), 
+    "guests" : [ { 
+      "phone" : 11111, "_id" : ObjectId("61ef629981f154bead6b0de4") 
+      } ], 
+    "created" : ISODate("2022-01-25T02:37:40.334Z"), 
+    "artist" : ObjectId("610f16a6ba0d5fc2d4593093"), 
+    "__v" : 1 
+}
